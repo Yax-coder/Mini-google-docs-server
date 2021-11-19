f19730bf3f70a48b37b8abe3b952f88ab978b571
yahaya <ahmadyahya962@gmail.com>
2021-11-19 06:45:30 +0100

    configured user cookies session

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,66 @@ import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
 
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from './entity/User';
+import { createAccessToken, createRefreshToken } from './auth';
+import { sendRefreshToken } from "./sendRefreshToken";
+
 
 (async () => {
-  const app = express();
- 
-  await createConnection();
+    const app = express();
+
+    app.use(cookieParser())
+    app.get("/", (_req, res) => res.send("hello"))
 
 
-  
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [UserResolver]
-    }),
-    context: ({ req, res }) => ({ req, res })
-  });
+    app.post("/refresh_token", async (req, res) => {
 
-  apolloServer.applyMiddleware({ app, cors: false });
+        const token = req.cookies.jid
+        if (!token) {
+            return res.send({ ok: false, accessToken: '' });
+        }
+        let payload: any = null;
+        try {
+            payload = verify(token, process.env.REFRESH_TOKEN_SECRECT!);
+        } catch (error) {
+            console.log(error);
+            return res.send({ ok: false, accessToken: '' });
+        }
 
-  app.listen(4000, () => {
-    console.log("express server started");
-  });
+        const user = await User.findOne({ id: payload.userId });
+
+        if (!user) {
+            return res.send({ ok: false, accessToken: '' });
+        }
+
+        if (user.tokenVersion !== payload.tokenVersion) {
+            return res.send({ ok: false, accessToken: '' });
+        }
+
+        sendRefreshToken(res, createRefreshToken(user))
+
+        return res.send({ ok: true, accessToken: createAccessToken(user) })
+
+    })
+
+    await createConnection();
+
+
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver]
+        }),
+        context: ({ req, res }) => ({ req, res })
+    });
+
+    apolloServer.applyMiddleware({ app, cors: false });
+
+    app.listen(4000, () => {
+        console.log("express server started");
+    });
 })();
 
 // createConnection().then(async connection => {
